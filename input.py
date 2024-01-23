c0df07261d1d239b9afb162a1ddf9b118bb9b7e6
Jessicachenyi <156784445+Jessicachenyi@users.noreply.github.com>
2024-01-23 12:08:31 +0000

    Create input.py

---
+++ input.py
@@ -0,0 +1,21 @@
+lunch = 100
+diner = 150
+one_week_fee =(lunch + diner)
+print (type(one_week_fee))
+monthly_fee = (16000)
+average_daily_fee=  monthly_fee/30
+print(type(average_daily_fee))
+
+print(type("hello world"))
+print("""
+      first class
+      first class
+      first class
+     """ )
+name= input("please type you name:")
+
+born_year= input("please type your birth")
+age= 2022 - int(born_year)
+print(age)
+#print ("hello "+name + ", you are born in "  + born_year +  " and you are " + str(age) + " years old.")
+print (f" hello {name}, you were born in {born_year}, and you are {age} years old.")
